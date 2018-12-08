552c08e279214b81fb7e585a41e329d1a3241fbe
ys1106 <yusun1162@gmail.com>
2018-12-09 01:36:15 +0900

    curriculum_manager 수정
    startapp들urls.py 추가 후 프로젝트 urlpatterns에 추가

--- curriculum_manager/urls.py
+++ curriculum_manager/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from curriculum.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('curriculum/', include('curriculum.urls', namespace='curriculum')),
+    path('user/', include('userinfo.urls', namespace='user')),
 ]
